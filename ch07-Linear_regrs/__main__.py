d340a33652a13977735760d883ee628e23cb44a5
hathackerwang <deanyuton@gmail.com>
2018-07-29 18:35:44 +0800

    ch07

---
+++ ch07-Linear_regrs/__main__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 14 15:02:16 2018
+
+@author: Administrator
+"""
+
+from Build_model import *
+from Forward_stepReg import *
+from Local_weightLR import *
+from Price_predict import *
+from Ridge_regre import *
+from Stand_Linear import *
+
+if __name__ == '__main__':
+    lgX=[]; lgY=[]
+    
